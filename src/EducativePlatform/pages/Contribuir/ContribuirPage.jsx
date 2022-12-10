4a6e0cb78cc52f0470d4a75f6567f5d9a81ea386
JavierLlanos6 <xavi6poop@gmail.com>
2022-12-10 17:51:56 -0400

    redireccion aportes

--- src/EducativePlatform/pages/Contribuir/ContribuirPage.jsx
+++ src/EducativePlatform/pages/Contribuir/ContribuirPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useFetch } from "../../../hooks";
 import { PracticeCard } from "../../components";
 import { Link } from "react-router-dom";
@@ -6,7 +6,23 @@ import styles from "./ContribuirPage.module.css";
 import PerfilU from "../MisAportes/foto/PerfilUsuario.jpg";
 import RevisarAporte from "../Contribuir/fotos/RevisarAporte.jpg";
 import SubirAporte from "../Contribuir/fotos/SubirAporte.jpg";
+import { useEffect, useState } from "react";
+import { get } from "../../helpers";
 export const ContribuirPage = () => {
+  const [aportes, setAportes] = useState([]);
+  const navigate = useNavigate();
+  const getAportes = async () => {
+    const id_usuario = localStorage.getItem("id_usuario");
+    console.log(id_usuario);
+    const data = await get(
+      "http://142.93.203.113:3001/api/contributions/?user=" + id_usuario
+    );
+    setAportes(data);
+    console.log(aportes);
+  };
+  useEffect(() => {
+    getAportes();
+  }, []);
   return (
     <div>
       <h1>Mis aportes</h1>
@@ -31,28 +47,27 @@ export const ContribuirPage = () => {
           <h1>Aportes recientes: </h1>
         </div>
       </Link>
-      
-        <div className={styles.Titulo}>
-        <Link to={`/clases/contribuir/Aporte`}>
-        <div className={styles.Class}>
-          <img src={PerfilU} alt="" className={styles.foto} />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe totam
-            obcaecati voluptas veniam dicta sed quasi commodi omnis unde.
-            Aliquam explicabo quas quibusdam perferendis sit impedit eum debitis
-            pariatur iusto!
-          </p>
-        </div>
-        </Link>
-        <div className={styles.Class}>
-          <img src={PerfilU} alt="" className={styles.foto} />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe totam
-            obcaecati voluptas veniam dicta sed quasi commodi omnis unde.
-            Aliquam explicabo quas quibusdam perferendis sit impedit eum debitis
-            pariatur iusto!
-          </p>
-        </div>
+
+      <div className={styles.Titulo}>
+        {/* <Link to={`/clases/contribuir/Aporte`}> */}
+        {aportes.map((aporte) => (
+          <div
+            className={styles.Class}
+            onClick={() => {
+              navigate("aporte/" + aporte.id);
+            }}
+          >
+            {/* <h3>{aporte.titulo}</h3> */}
+            <img src={PerfilU} alt="" className={styles.foto} />
+            <p>
+              {aporte.titulo}
+              <h3>{aporte.nombre_completo}</h3>
+
+              {aporte.descripcion}
+              {/* {aporte.descripcion}{aporte.id} */}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
